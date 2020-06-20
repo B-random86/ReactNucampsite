5089bbeef5a3b639b18d17f11c50045c8773a6e0
Brandon <eosguitarist27@gmail.com>
2020-06-20 15:20:18 -0700

    feedback submit

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem,
-    Button, Label, Col, Row, FormFeedback } from 'reactstrap';
+    Button, Label, Col, Row, } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, Form, Errors } from 'react-redux-form';
 
